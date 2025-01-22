4b6467b6e8bf34a23cba4a498181d58b577269ba
cledwin <cledwin@cakeplabs.com>
2025-01-23 01:30:36 +0800

    gas

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,109 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { AnimatePresence, motion } from "framer-motion"
+import { useState } from "react"
+
+export default function Page() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="relative w-full h-screen overflow-hidden">
+      {/* Main Content */}
+      <div className="w-full h-full relative">
+        {/* Background Image */}
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: `url('./bw.jpeg')`,
+            filter: "brightness(0.7)",
+          }}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        {/* Content Overlay */}
+        <div className="relative h-full flex flex-col items-center justify-start text-center px-4 md:px-8 lg:px-16">
+          <div className="mt-2 sm:mt-8" />
+          <motion.h1
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              delay: 0.6,
+              duration: 1,
+              ease: "easeOut",
+            }}
+            className="text-4xl md:text-8xl lg:text-[80px] font-extrabold font-luckiest-guy-bold drop-shadow-md text-yellow-300 mb-2 sm:mb-6 md:mb-8"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            HALO TEMAN-TEMAN !
+          </motion.h1>
+
+          <motion.p
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              delay: 1.2,
+              duration: 1,
+              ease: "easeOut",
+            }}
+            className="max-w-5xl text-yellow-300 text-[10px] sm:text-sm"
           >
-            Read our docs
-          </a>
+            Perjalanan kalian di AMSA adalah langkah luar biasa menuju masa depan yang gemilang. Tetap semangat dalam
+            setiap kegiatan, kolaborasi, dan kontribusi kalian. Ingat, setiap usaha kecil yang kalian lakukan akan
+            membawa dampak besar, baik untuk diri sendiri, tim, maupun masyarakat. Jangan pernah takut untuk bermimpi
+            besar dan melangkah maju, karena di sinilah kalian belajar, tumbuh, dan menginspirasi. Kalau rintangan
+            datang, hadapi dengan keberanian dan senyuman, karena kalian adalah generasi pemimpin yang penuh potensi.
+          </motion.p>
+          <motion.p
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              delay: 1.8,
+              duration: 1,
+              ease: "easeOut",
+            }}
+            className="max-w-5xl w-full text-end text-[10px] sm:text-sm text-yellow-300 italic font-bold mt-4 mb-0 sm:mb-8"
+          >
+            Sumba Volunteer - 2025
+          </motion.p>
+
+          <motion.div
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 2.4, duration: 1, ease: "easeOut" }}
+          >
+            <button
+              onClick={() => setShowModal(true)}
+              className="bg-white hover:bg-yellow-300/80 text-black hover:text-white font-normal text-[10px] sm:text-sm px-8 py-1 rounded-full"
+            >
+              Pesan dari Kami
+            </button>
+          </motion.div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      {/* Fullscreen Modal */}
+      <AnimatePresence>
+        {showModal && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="fixed inset-0 z-50 bg-black"
+          >
+            <button
+              onClick={() => setShowModal(false)}
+              className="absolute top-4 right-4 z-50 text-white hover:bg-white/10"
+            >
+              <p className="size-10">X</p>
+            </button>
+
+            <video autoPlay controls className="w-full h-full object-cover">
+              <source src="/final_video.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
-  );
+  )
 }
+
